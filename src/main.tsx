62fdda46b63db9f931611bbe399638c78c5ae9d5
pbakabane <akabane@primebrains.co.jp>
2025-05-14 15:16:15 +0900

    feature/3 Apps.tsxの削除、Article.tsxの修正、main.tsxの修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Router } from './router/Router.tsx';
+import { Router } from './router/Router';
 import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
