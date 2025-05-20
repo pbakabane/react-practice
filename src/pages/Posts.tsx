078fb1b188a2b09bfb11741770890a30882363b0
pbakabane <akabane@primebrains.co.jp>
2025-05-20 13:17:31 +0900

    feature/13 Posts.tsx現状共有

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -12,34 +12,46 @@ export const Posts = () => {
   const navigate = useNavigate();
   const [selectedPostId, setSelectedPostId] = useState<number | null>(null);
 
-  const { data: posts = [] } = useQuery<Post[], Error>({
+  const {
+    data: posts = [],
+    error,
+    isPending,
+    isFetching
+  } = useQuery<Post[], Error>({
     queryKey: ["posts"],
     queryFn: async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      if (!res.ok) throw new Error("Failed to fetch posts");
+      const res = await fetch("https://jsonplaceholder.typicode.com/err"); //https://jsonplaceholder.typicode.com/posts
       return res.json();
     }
   });
 
-  const handleClick = (post: Post) => {
-    setSelectedPostId(post.id);
-    navigate(`/posts/${post.id}`, {
-      state: post
+  console.log("https://jsonplaceholder.typicode.com/err"); //実行時URL
+  console.log("postsの中身:", posts);
+  console.log("errorの中身:", error);
+
+  const handleClick = (posts: Post) => {
+    setSelectedPostId(posts.id);
+    navigate(`/posts/${posts.id}`, {
+      state: posts
     });
   };
 
   return (
     <div style={{ padding: "20px" }}>
       <h3>Here is posts!</h3>
-      {posts.map((post: Post) => (
-        <div key={post.id}>
-          <h2 onClick={() => handleClick(post)}>{post.title}</h2>
-          {selectedPostId === post.id && (
-            <Outlet />
-            // <p style={{ color: 'gray' }}>{post.body}</p>
-          )}
-        </div>
-      ))}
+
+      {isFetching || isPending ? (
+        <div style={{ color: "yellow" }}>loading...</div>
+      ) : error ? (
+        <div style={{ color: "red" }}>エラーです</div>
+      ) : (
+        posts.map((posts: Post) => (
+          <div key={posts.id}>
+            <h2 onClick={() => handleClick(posts)}>{posts.title}</h2>
+            {selectedPostId === posts.id && <Outlet />}
+          </div>
+        ))
+      )}
     </div>
   );
 };
