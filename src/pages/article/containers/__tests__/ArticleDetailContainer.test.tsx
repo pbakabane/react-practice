89d00ae79447648c8cab5b5ad4f76ac629faafd7
pbakabane <akabane@primebrains.co.jp>
2025-05-28 15:25:58 +0900

    feature/17 /article関連の単体テスト実装・状況共有

--- src/pages/article/containers/__tests__/ArticleDetailContainer.test.tsx
+++ src/pages/article/containers/__tests__/ArticleDetailContainer.test.tsx
@@ -23,23 +23,31 @@ describe("ArticleDetailContainerのテスト", () => {
     jest.clearAllMocks();
   });
 
-  test("現在 /article/2 で、from が /article/1 の場合", () => {
-    mockedUseParams.mockReturnValue({ id: "2" });
-    mockedUseLocation.mockReturnValue({ state: { from: "/article/1" } });
-
-    render(<Wrapper />);
-
-    expect(screen.getByText("This is article 2")).toBeInTheDocument();
-    expect(screen.getByText("from: 1")).toBeInTheDocument();
+  describe("現在 /article/2 で、from が /article/1 の場合", () => {
+    beforeEach(() => {
+      mockedUseParams.mockReturnValue({ id: "2" });
+      mockedUseLocation.mockReturnValue({ state: { from: "/article/1" } });
+      render(<Wrapper />);
+    });
+    test("「This is article 2」が表示されていること", () => {
+      expect(screen.getByText("This is article 2")).toBeInTheDocument();
+    });
+    test("遷移元の画面の情報が表示されていること", () => {
+      expect(screen.getByText("from: 1")).toBeInTheDocument();
+    });
   });
 
-  test("現在 /article/1 で、from が / の場合", () => {
-    mockedUseParams.mockReturnValue({ id: "1" });
-    mockedUseLocation.mockReturnValue({ state: { from: "/" } });
-
-    render(<Wrapper />);
-
-    expect(screen.getByText("This is article 1")).toBeInTheDocument();
-    expect(screen.queryByText(/^from:/)).not.toBeInTheDocument();
+  describe("現在 /article/1 で、from が / の場合", () => {
+    beforeEach(() => {
+      mockedUseParams.mockReturnValue({ id: "1" });
+      mockedUseLocation.mockReturnValue({ state: { from: "/" } });
+      render(<Wrapper />);
+    });
+    test("「This is article 1」が表示されていること", () => {
+      expect(screen.getByText("This is article 1")).toBeInTheDocument();
+    });
+    test("遷移元の画面の情報が表示されていないこと", () => {
+      expect(screen.queryByText(/^from:/)).not.toBeInTheDocument();
+    });
   });
 });
